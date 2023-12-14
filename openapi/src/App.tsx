078a9712bb0c9843d3656cf058706c545c928c93
light-planck <seseifunc16@gmail.com>
2023-12-15 00:54:04 +0900

    chore: yamlファイルのurlをenvから取得する

--- openapi/src/App.tsx
+++ openapi/src/App.tsx
@@ -2,12 +2,9 @@ import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
 const App = () => {
-  // const origin = import.meta.env.VITE_APP_ORIGIN;
-
   return (
     <div className="App">
-      {/* <SwaggerUI url={`${origin}/doc/openapi.yaml`} /> */}
-      <SwaggerUI url={'https://light-planck.github.io/nemmy/openapi/openapi.yaml'} />
+      <SwaggerUI url={import.meta.env.VITE_YAML_FILE_URL} />
     </div>
   );
 };
