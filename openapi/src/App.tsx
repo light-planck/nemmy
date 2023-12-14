8dd25a0d7ec365ad097a185439518f420c6578d1
light-planck <seseifunc16@gmail.com>
2023-12-14 23:45:02 +0900

    chore: originを指定

--- openapi/src/App.tsx
+++ openapi/src/App.tsx
@@ -2,9 +2,11 @@ import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
 const App = () => {
+  const origin = import.meta.env.VITE_APP_ORIGIN;
+
   return (
     <div className="App">
-      <SwaggerUI url={`/doc/openapi.yaml`} />
+      <SwaggerUI url={`${origin}/doc/openapi.yaml`} />
     </div>
   );
 };
