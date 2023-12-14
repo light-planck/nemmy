554396f45827474c8fdbd98260ee4cbcce15e114
light-planck <seseifunc16@gmail.com>
2023-12-14 23:10:59 +0900

    fix: 相対パスで指定する

--- openapi/src/App.tsx
+++ openapi/src/App.tsx
@@ -2,11 +2,9 @@ import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
 const App = () => {
-  const origin = import.meta.env.VITE_APP_ORIGIN
-
   return (
     <div className="App">
-      <SwaggerUI url={`${origin}/openapi.yaml`} />
+      <SwaggerUI url={`docs/openapi.yaml`} />
     </div>
   );
 };
