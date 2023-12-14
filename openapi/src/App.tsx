f5663f3369af6fcf1be3e7b0680c4ac84f5f1f0b
light-planck <seseifunc16@gmail.com>
2023-12-15 00:45:06 +0900

    chore: github pagesにしてみる

--- openapi/src/App.tsx
+++ openapi/src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div className="App">
       {/* <SwaggerUI url={`${origin}/doc/openapi.yaml`} /> */}
-      <SwaggerUI url={`.vercel/output/static/openapi.yaml`} />
+      <SwaggerUI url={'https://light-planck.github.io/nemmy/openapi/openapi.yaml'} />
     </div>
   );
 };
