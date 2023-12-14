9e102a79e9bee915a4d927390c8c0e35c9a50ca5
light-planck <seseifunc16@gmail.com>
2023-12-15 00:27:02 +0900

    fix: vercelで静的ファイルをホストする

--- openapi/src/App.tsx
+++ openapi/src/App.tsx
@@ -2,11 +2,12 @@ import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
 const App = () => {
-  const origin = import.meta.env.VITE_APP_ORIGIN;
+  // const origin = import.meta.env.VITE_APP_ORIGIN;
 
   return (
     <div className="App">
-      <SwaggerUI url={`${origin}/doc/openapi.yaml`} />
+      {/* <SwaggerUI url={`${origin}/doc/openapi.yaml`} /> */}
+      <SwaggerUI url={`.vercel/output/static/openapi.yaml`} />
     </div>
   );
 };
