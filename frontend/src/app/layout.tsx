c85e12ae08c8a34507e3ff07782fc0e2098bb8aa
light-planck <seseifunc16@gmail.com>
2024-01-20 10:35:34 +0900

    chore: shadcn-uiを導入

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ja" className="dark">
+    <html lang="ja">
       <body>{children}</body>
     </html>
   );
