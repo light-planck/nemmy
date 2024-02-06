ce0fcc29d6fa67cc80f4198e65a2e9e4fb587ea6
ynm3n <43065779+ynm3n@users.noreply.github.com>
2024-02-06 22:49:07 +0900

    refactor: Config型からDSN以外のDB情報のフィールドを削除
    DSN以外のDB情報はプログラム中で使用されない。そのため、GetConfig関数内でのみそれらを扱うようにした

--- backend/internal/config.go
+++ backend/internal/config.go
@@ -7,10 +7,6 @@ import (
 )
 
 type Config struct {
-	DBUser     string `env:"POSTGRES_USER" envDefault:"postgres"`
-	DBPassword string `env:"POSTGRES_PASSWORD,required"`
-	DBName     string `env:"POSTGRES_DB" envDefault:"postgres"`
-
 	Port        string `env:"APP_CONTAINER_PORT,required"`
 	DatabaseDSN string `env:"POSTGRES_DSN"`
 	FrontendURL string `env:"FRONTEND_URL,required"`
@@ -22,7 +18,15 @@ func GetConfig() (*Config, error) {
 		return nil, fmt.Errorf("GetConfig: %w", err)
 	}
 	if len(cfg.DatabaseDSN) == 0 {
-		cfg.DatabaseDSN = fmt.Sprintf("postgres://%[1]v:%[2]v@db/%[3]v?sslmode=disable", cfg.DBUser, cfg.DBPassword, cfg.DBName)
+		dbEnv := struct {
+			User     string `env:"POSTGRES_USER" envDefault:"postgres"`
+			Password string `env:"POSTGRES_PASSWORD,required"`
+			Name     string `env:"POSTGRES_DB" envDefault:"postgres"`
+		}{}
+		if err := env.Parse(&dbEnv); err != nil {
+			return nil, fmt.Errorf("GetConfig: %w", err)
+		}
+		cfg.DatabaseDSN = fmt.Sprintf("postgres://%[1]v:%[2]v@db/%[3]v?sslmode=disable", dbEnv.User, dbEnv.Password, dbEnv.Name)
 	}
 	return cfg, nil
 }
