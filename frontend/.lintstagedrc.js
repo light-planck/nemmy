a3998fa6b47fefad07e905768b34f50711c5952a
light-planck <seseifunc16@gmail.com>
2024-03-15 22:04:39 +0900

    chore: lint-stagedでprettierを実行する

--- frontend/.lintstagedrc.js
+++ frontend/.lintstagedrc.js
@@ -5,6 +5,12 @@ const buildEslintCommand = (filenames) =>
     .map((f) => path.relative(process.cwd(), f))
     .join(" --file ")}`;
 
+const buildPrettierCommand = (filenames) =>
+  `prettier --write --ignore-unknown ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" ")}`;
+
 module.exports = {
   "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+  "*": [buildPrettierCommand],
 };
