1f14ea1e87de3cd787303c924f7f5b117e1fea82
light planck <55218925+light-planck@users.noreply.github.com>
2024-03-17 01:31:49 +0900

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 
 import "./globals.css";
 
-export const runtime = "edge"
-
 export const metadata: Metadata = {
   title: "nemmy",
   description: "nemmy is an awesome web app",
