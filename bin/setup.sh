c106317fdfa6297b0acc840822014b5cfe057683
light-planck <seseifunc16@gmail.com>
2024-03-17 13:08:42 +0900

    chore: 初回セットアップ用スクリプトの作成

---
+++ bin/setup.sh
@@ -0,0 +1,31 @@
+#!/bin/zsh
+
+check_requrired_command() {
+  for cmd in "$@"
+  do
+    if ! command -v $cmd &> /dev/null
+    then
+      echo -e "\033[0;31mplease install $cmd!\033[0m"
+      exit 1
+    fi
+  done
+}
+
+install_npm_dependencies() {
+  for dir in "$@"
+  do
+    (
+      cd $dir
+      pnpm install --frozen-lockfile
+    ) &
+  done
+  wait
+}
+
+main() {
+  check_requrired_command lefthook pnpm
+  lefthook install
+  install_npm_dependencies frontend swagger
+}
+
+main
