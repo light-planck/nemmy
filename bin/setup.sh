0c7cd1b9a1789ceee8be67564f972a95bb675056
light-planck <seseifunc16@gmail.com>
2024-03-18 20:18:01 +0900

    chore: 必須コマンドを追加

--- bin/setup.sh
+++ bin/setup.sh
@@ -23,7 +23,7 @@ install_npm_dependencies() {
 }
 
 main() {
-  check_requrired_command lefthook pnpm
+  check_requrired_command lefthook pnpm task docker
   lefthook install
   install_npm_dependencies frontend swagger
 }
