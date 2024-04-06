1802494042cf8cd95d9dce477e4a55c28038ec36
light-planck <seseifunc16@gmail.com>
2024-04-06 17:23:58 +0900

    feat: ヘッダーの作成

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
+import { Header } from "@/components/layouts/header";
 
 export const metadata: Metadata = {
   title: "nemmy",
@@ -14,7 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body>
+        <Header />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
---
+++ frontend/src/components/layouts/header.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@radix-ui/react-navigation-menu";
+import Link from "next/link";
+
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { navigationMenuTriggerStyle } from "../ui/navigation-menu";
+import { Separator } from "../ui/separator";
+import { Skeleton } from "../ui/skeleton";
+
+const navigationItems = [
+  { href: "/", label: "ホーム" },
+  { href: "/subjects", label: "教材一覧" },
+] as const;
+
+export const Header = () => {
+  return (
+    <>
+      <header className="flex p-5 gap-2 justify-between">
+        <h1 className="text-2xl">nemmy</h1>
+        <NavigationMenu>
+          <NavigationMenuList className="flex">
+            {navigationItems.map((item) => (
+              <NavigationMenuItem key={item.href}>
+                <Link href={item.href} legacyBehavior passHref>
+                  <NavigationMenuLink
+                    className={`text-base ${navigationMenuTriggerStyle()}`}
+                  >
+                    {item.label}
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            ))}
+          </NavigationMenuList>
+        </NavigationMenu>
+        <Avatar>
+          <AvatarImage src="https://github.com/light-planck.png" />
+          <AvatarFallback>
+            <Skeleton className="h-12 w-12 rounded-full" />
+          </AvatarFallback>
+        </Avatar>
+      </header>
+      <Separator />
+    </>
+  );
+};
